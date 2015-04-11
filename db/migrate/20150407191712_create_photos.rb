e6b0f4899ac0c30f80e8f44a5de23204fa90c640
Kevin <kevin.delme@gmail.com>
2015-04-11 14:36:30 +0200

    heroku db still broken

--- db/migrate/20150407191712_create_photos.rb
+++ db/migrate/20150407191712_create_photos.rb
@@ -2,6 +2,7 @@ class CreatePhotos < ActiveRecord::Migration
   def change
     create_table :photos do |t|
 	  t.string :title
+	  t.integer :work_id
 	  t.belongs_to :work, index: true
 
       t.timestamps null: false
--- db/migrate/20150410175723_add_work_to_photos.rb
+++
@@ -1,5 +0,0 @@
-class AddWorkToPhotos < ActiveRecord::Migration
-  def change
-    add_column :photos, :work_id, :integer
-  end
-end
